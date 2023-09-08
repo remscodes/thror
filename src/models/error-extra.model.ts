13ff8f3b95b4c880f9083b67604cd555c15f96cc
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-09-08 19:52:16 +0200

    Change ErrorExtra.original type from Error to any

--- src/models/error-extra.model.ts
+++ src/models/error-extra.model.ts
@@ -2,6 +2,6 @@ import type { Indexed } from './shared.model';
 
 export interface ErrorExtra extends Indexed {
   withStack?: boolean;
-  errorCode?: string | number;
-  original?: Error;
+  original?: any;
+  // errorCode?: string | number;
 }
